c5ce21cccc7e4fdec29cd3d94099fccf3af50bf3
RITESH MORE <moreritesh3969@gmail.com>
2025-05-20 17:16:17 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+  
+  return (
+    <>
+     
+
+      <div class="h-[300vh] bg-red-500">
+  
+      <Navbar/>   
+
+
+
+      fhfhfhfhf rnf
+
+
+
+
+
+      redfsrekdjg
+      bg 
+
+
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem aut nesciunt doloremque, adipisci vel facilis aliquam! Porro hic eius nemo aliquam perferendis quas est ratione sequi veritatis mollitia ipsam sapiente aliquid amet nesciunt, minus repudiandae consectetur! Dolore, fugiat architecto aspernatur cupiditate dicta repudiandae non, incidunt harum officiis a molestias qui ad quae alias doloremque necessitatibus. Velit minima, dolorum debitis nihil nam earum iure inventore repudiandae, error, laudantium at. Nam blanditiis asperiores ducimus consequatur tempora debitis aperiam, sed, voluptas eius explicabo possimus aut corporis, quas earum? Rerum nihil animi labore, iste omnis non dolor placeat consequuntur cum, optio, iure suscipit necessitatibus? Vero consectetur minus earum quibusdam iure exercitationem, inventore sunt esse modi expedita fuga nemo ut itaque? Sed nam veritatis quasi qui reprehenderit ducimus saepe facilis dicta repudiandae aliquam enim, numquam vero eum corporis temporibus aliquid itaque illo distinctio vitae? Cupiditate illo obcaecati aliquid doloremque illum sed ipsam velit, eius quod saepe harum corporis unde impedit delectus esse reprehenderit in molestiae atque, numquam ad nobis culpa. Dolor aperiam necessitatibus minima tempore architecto alias ut suscipit eaque adipisci! Doloremque quae explicabo quisquam, facere nulla consequatur. Alias necessitatibus aliquam suscipit, porro libero illum at soluta. Repudiandae possimus totam dolores ab odit quisquam consequuntur ratione suscipit, facere quasi illum fugit officia vitae qui, beatae reprehenderit recusandae nemo illo eaque impedit eum natus. Corporis quas exercitationem vel earum, aspernatur fuga optio impedit at dolorum molestiae amet, expedita quidem accusamus ipsam voluptatibus quaerat. Quod aspernatur aperiam debitis? Explicabo sunt a voluptatibus deserunt architecto expedita, ipsum eius sapiente culpa tenetur et dolorem laborum! Inventore corrupti fuga sapiente amet cum beatae itaque repellendus accusantium voluptatem aspernatur suscipit dolore, consequuntur nam mollitia laboriosam nemo error? Excepturi autem eaque delectus sint maiores quidem quisquam, deserunt laborum! Debitis ipsam quasi molestiae minus qui eligendi id itaque sapiente. Quidem atque aut quia voluptate fuga, cum quod quo cumque voluptatem incidunt autem non sint eveniet laborum quos officiis porro doloremque, possimus culpa. Aperiam rem quos, architecto ut cupiditate eligendi autem, ipsam facere placeat recusandae fugiat quasi, iusto nobis optio assumenda. Qui reprehenderit amet fuga consequatur. At quia aliquam alias iste deleniti quos reprehenderit molestias, sit, ea dicta ex totam laboriosam temporibus quibusdam, inventore saepe animi veniam cum laborum neque. Nisi officia esse ullam ut quidem voluptas error et, quod voluptatibus quisquam ad enim rerum aliquid repellendus. Quaerat placeat consequatur optio. Recusandae fuga non, temporibus quas reprehenderit quos nisi suscipit placeat ipsam culpa, autem neque obcaecati laboriosam sint tenetur. Sint, nobis sed, molestias consectetur sapiente corporis nisi, hic architecto distinctio molestiae quibusdam laudantium? At doloremque voluptates vitae perferendis adipisci repellendus iusto aspernatur. Quisquam, architecto harum tempore doloribus consectetur facere quidem et? Deserunt deleniti quasi facere eius, praesentium excepturi tempora? Quae error in quo nam, consequatur vero aperiam hic porro fuga nostrum culpa, possimus facilis suscipit magni sit atque esse nesciunt rerum! Distinctio obcaecati est incidunt, cumque molestias, velit veritatis deserunt quae non tempore aut commodi reiciendis quia voluptate dolorem officiis corrupti numquam quo! Est a nesciunt laboriosam sit aut natus sed eius quisquam laborum?
+</div>
+
+    
+    </>
+  )
+}
+
+export default App
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,105 @@
+import React, { useState } from "react";
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="overflow-y-auto  sticky top-0 z-50  ">
+      <nav className="border-gray-200 bg-[rgba(255,255,255)] m-12 rounded-3xl shadow-md">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          
+          {/* Logo / Brand */}
+          <span className="text-3xl hover:tracking-widest hover:scale-125 ease-in-out duration-200 cursor-pointer text-[#4B0082] font-bold">
+            EduTech
+          </span>
+
+          {/* Mobile menu button */}
+          <button
+            onClick={toggleMenu}
+            type="button"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center rounded-lg md:hidden hover:bg-white focus:outline-none focus:ring-2 focus:ring-skyline-blue"
+            aria-controls="navbar-default"
+            aria-expanded={isOpen ? "true" : "false"}
+          >
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="w-5 h-5 text-skyline-blue"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 17 14"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M1 1h15M1 7h15M1 13h15"
+              />
+            </svg>
+          </button>
+
+          {/* Navbar Links */}
+          <div
+            className={`${isOpen ? "block" : "hidden"} w-full md:block md:w-auto`}
+            id="navbar-default"
+          >
+            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
+              
+              <li>
+                <a
+                  href="/"
+                  className="block py-2 px-3 rounded cursor-pointer md:bg-transparent hover:bg-cobalt-depth hover:text-pure-white ease-in-out duration-300"
+                >
+                  Home
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="/courses"
+                  className="block py-2 px-3 rounded cursor-pointer md:bg-transparent hover:bg-cobalt-depth hover:text-pure-white ease-in-out duration-300"
+                >
+                  Courses
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="/student"
+                  className="block py-2 px-3 rounded cursor-pointer md:bg-transparent hover:bg-cobalt-depth hover:text-pure-white ease-in-out duration-300"
+                >
+                  Profile
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="/teacher"
+                  className="block py-2 px-3 rounded cursor-pointer md:bg-transparent hover:bg-cobalt-depth hover:text-pure-white ease-in-out duration-300"
+                >
+                  Teacher
+                </a>
+              </li>
+
+              <li className="bg-pure-white rounded-md hover:bg-golden-ember">
+                <a
+                  href="/login"
+                  className="block py-2 px-3 rounded cursor-pointer ease-in-out duration-300 text-cobalt-depth hover:text-pure-white"
+                >
+                  Login
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
